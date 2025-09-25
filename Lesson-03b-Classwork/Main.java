610cf1fdc6999253cb20486dec80fecd16ab0dfb
anabia-f <anabiaf@nycstudents.net>
2025-09-25 14:13:37 +0000

    update

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+       System.out.println("Anabia Farooq");
+       System.out.println("150-21 75th Ave");
+       System.out.println("Anabia Farooq \n150-21 75th Ave");
+  }
+
+  
+}
