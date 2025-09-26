d8c82d69c5e6910545193febd5053a2458fb0f70
anabia-f <anabiaf@nycstudents.net>
2025-09-26 00:46:11 +0000

    lesson7

--- Lesson-07-activity/Main.java
+++ Lesson-07-activity/Main.java
@@ -9,7 +9,7 @@ class Main {
     Challenge 1:
     1) Create the variables, ask the user for the variable values, write the equation in file EQ1-act6 and display the equation value.
 */
-  System.out.println("enter x");
+  System.out.println("enter y");
   double x = Input.readDouble();
   double y = 0;
   y = Math.pow(x,7);
@@ -18,14 +18,23 @@ class Main {
     Challenge 2:
     1) Create the variables, ask the user for the variable values, write the equation in fileEQ1.1-act6 and display the equation value.
 */
-  
+  System.out.println("enter q");
+  double z = Input.readDouble();
+  double q = 0;
+  q = Math.pow(z,3) + 5.0;
+  System.out.println(q);
 
 /*  
     Challenge 3:
     Create the variables, ask the user for the variable values, write the equation in file EQ2-act6 and display the equation value..
     
 */
-
+  System.out.println("enter s");
+  double t = Input.readDouble();
+  double r = Input.readDouble();
+  double s = 0;
+  s = Math.pow(r,5) * Math.pow(r + 2, 4);
+  System.out.println(s);
  
 
 /*  
@@ -33,7 +42,13 @@ class Main {
     Create the variables, ask the user for the variable values, write the equation in file EQ3-act6 and display the equation value..
     
 */
-
+  System.out.println("enter C");
+  double A = Input.readDouble();
+  double B = Input.readDouble();
+  double C = 0;
+  C = Math.sqrt(A+B);
+  System.out.println(C);
+ 
 
 
 /*  
@@ -42,7 +57,15 @@ class Main {
     
 */
 
-
+  System.out.println("enter d");
+  double x1 = Input.readDouble();
+  double x2 = Input.readDouble();
+  double y1 = Input.readDouble();
+  double y2 = Input.readDouble();
+  double d = 0;
+  d = Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+  System.out.println(d);
+ 
 
 
 /*  
@@ -51,6 +74,12 @@ class Main {
     
 */
 
+  System.out.println("enter g");
+  double deg = Input.readDouble();
+  double g = 0;
+  g = Math.sin(Math.toRadians(deg));
+  System.out.println(g);
+ 
 
 
 
@@ -61,7 +90,13 @@ class Main {
     
 */
 
-
+  System.out.println("enter k");
+  double m = Input.readDouble();
+  double n = Input.readDouble();
+  double k = 0;
+  k = Math.pow(m,5) / Math.sqrt(n+1);
+  System.out.println(k);
+ 
 
 
 /*  
@@ -72,6 +107,7 @@ class Main {
 */
 
 
+ 
 
 
 
